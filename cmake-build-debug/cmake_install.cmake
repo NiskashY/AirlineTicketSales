7c033891c2196f26d1c40e1e1536adf78cfc25c9
NiskashY <roman.pantus@mail.ru>
2022-04-16 18:09:38 +0300

    some set ups on the desktop

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mint/CLionProjects/AirlineTicketSales
+# Install script for directory: /home/mint/CLionProjects/AirlineTicketSaless
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mint/CLionProjects/AirlineTicketSales/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mint/CLionProjects/AirlineTicketSaless/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
