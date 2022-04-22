61e6f179a366fa5fa4fa162cb8409a93a700c28f
NiskashY <roman.pantus@mail.ru>
2022-04-22 19:19:13 +0300

    project configuration

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "data/flight.h"
+#include "data/reader.h"
+#include <ncurses.h>
+//#include "user/user.h"
 
 int main() {
+    int n = 0;
+    std::cin >> n;
+    AddSeveralFlights(n);
+    std::cout << '\n';
+    auto vec = ReadFromFile();
+
+    for (auto& i : vec) {
+//        i.ShowFlight();
+        std::cout << std::left << i << '\n';
+    }
+
 
     return 0;
 }
